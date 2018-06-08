a0a504986fef1aaae62955a306c6afb39e832ce6
alex-avila <alex.avilx@icloud.com>
2018-06-07 21:18:14 -0600

    Finished most of the functionality of the game.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
 const grid = document.getElementById('grid')
+let firstCard = null
+let secondCard = null
+let isChecking = false
+// when sets matched equals 8
+// win
+let setsMatched = 0
+let win = false
 
-const generateCards = () => {
+
+// Functions
+const generateCards = len => {
+    // Generates 16 cards
+    // every card has a pair that has the same data-name
     let html = ''
-    for (let i = 0; i < 16; i++) {
-        html += `<div class="card" data-name="hello">` + 
-            `<div class="card__side card__front"></div>` + 
+    let x = [...Array(len / 2).keys()]
+    for (let i = 0; i < len; i++) {
+        const randIndex = Math.floor(Math.random() * x.length)
+        const selection = x.splice(randIndex, 1)
+        if (i === 7) {
+            x = [...Array(len / 2).keys()]
+        }
+        html += `<div class="card" data-name="${selection}">` + 
+            `<div class="card__side card__front">${selection}</div>` + 
             `<div class="card__side card__back"></div>` +
             `</div>`
     }
@@ -13,11 +30,47 @@ const generateCards = () => {
 
 const flip = e => {
     const card = e.path[1]
-    card.classList.toggle('flipped')
+    if (!card.dataset.matched && !isChecking) {
+        if (!firstCard) {
+            card.classList.toggle('flipped')
+            firstCard = card
+        } else if (card !== firstCard) {
+            card.classList.toggle('flipped')
+            secondCard = card
+            isChecking = true
+            setTimeout(checkMatch, 600)
+        }
+    }
 }
 
-generateCards()
+const checkMatch = () => {
+    if (firstCard.dataset.name === secondCard.dataset.name) {
+        firstCard.dataset.matched = true
+        secondCard.dataset.matched = true
+        setsMatched++
+        console.log(setsMatched)
+        if (setsMatched === 8) {
+            win = true
+            console.log('You won')
+            // win function
+        }
+        // animate cards (match!)
+    } else {
+        firstCard.classList.toggle('flipped')
+        secondCard.classList.toggle('flipped')
+        // animate cards (noMatch!)
+    }
+    firstCard = null
+    secondCard = null
+    isChecking = false
+}
 
+
+// Generate cards when page loads
+generateCards(16)
+
+
+// Add Event Listener to all Cards
 for (let card of document.getElementsByClassName('card')) {
     card.addEventListener('click', flip)
 }
