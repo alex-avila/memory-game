6cea338fc1b32f02464665a0add341c3a08d370f
alex-avila <alex.avilx@icloud.com>
2018-06-16 17:01:56 -0600

    Added semicolons to adhere to the udacity style guide

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const grid = document.getElementById('grid'),
     modal = document.getElementById('modal'),
     playAgain = document.getElementById('play-again'),
     timeResult = document.getElementById('time-result'),
-    starResult = document.getElementById('star-result')
+    starResult = document.getElementById('star-result');
 
 // Global game state
 let gameState = {
@@ -21,10 +21,10 @@ let gameState = {
     timer: new Date(0),
     stars: 3,
     interval: null
-}
+};
 
 // Initial game state for resetting
-const initialGameState = { ...gameState }
+const initialGameState = { ...gameState };
 
 const generateCards = () => {
     const icons = [
@@ -36,200 +36,202 @@ const generateCards = () => {
         'ra-eggplant',
         'ra-chicken-leg',
         'ra-super-mushroom'
-    ]
-    let html = ''
-    let iconsArr = [...icons]
+    ];
+    let html = '';
+    let iconsArr = [...icons];
 
     for (let i = 0; i < 16; i++) {
-        const randIndex = Math.floor(Math.random() * iconsArr.length)
-        const selection = iconsArr.splice(randIndex, 1)
+        const randIndex = Math.floor(Math.random() * iconsArr.length);
+        const selection = iconsArr.splice(randIndex, 1);
         if (i === 7) {
-            iconsArr = [...icons]
+            iconsArr = [...icons];
         }
         html += `<div class="card" data-name="${selection}">` +
             `<div class="card__side card__back"></div>` +
             `<div class="ra ${selection} card__side card__front"></div>` +
-            `</div>`
+            `</div>`;
     }
 
-    grid.innerHTML = html
+    grid.innerHTML = html;
     
     // Add event listener to each card
     for (let card of document.getElementsByClassName('card')) {
-        card.addEventListener('click', flip)
+        card.addEventListener('click', flip);
     }
-}
+};
 
 const flip = e => {
     // It's possible to click a card side or the parent card element
     // This ensures that the card variable selected is always correct
-    const card = e.path.length === 9 ? e.path[0] : e.path[1]
+    const card = e.path.length === 9 ? e.path[0] : e.path[1];
 
     if (gameState.moves === 0) {
-        startTimer()
+        startTimer();
     }
 
     if (!card.dataset.matched && !gameState.isChecking) {
         if (!gameState.firstCard) {
-            updateMovesAndStars(++gameState.moves)
-            card.classList.toggle('flipped')
-            gameState.firstCard = card
+            updateMovesAndStars(++gameState.moves);
+            card.classList.toggle('flipped');
+            gameState.firstCard = card;
         } else if (card !== gameState.firstCard) {
-            card.classList.toggle('flipped')
-            gameState.secondCard = card
-            gameState.isChecking = true
-            setTimeout(checkMatch, 550)
+            card.classList.toggle('flipped');
+            gameState.secondCard = card;
+            gameState.isChecking = true;
+            setTimeout(checkMatch, 550);
         }
     }
-}
+};
 
 const win = () => {
-    clearInterval(gameState.interval)
-    const time = gameState.timer
-    const defaultText = time.getSeconds() + '<span class="time-unit">s</span>'
+    clearInterval(gameState.interval);
+    const time = gameState.timer;
+    const defaultText = time.getSeconds() + '<span class="time-unit">s</span>';
     timeResult.innerHTML = time.getMinutes() ?
         `${time.getMinutes()}<span class="time-unit">m</span> ${defaultText}` :
-        defaultText
-    starResult.textContent = `${gameState.stars} out of ${initialGameState.stars}`
-    showModal()
-}
+        defaultText;
+    starResult.textContent = `${gameState.stars} out of ${initialGameState.stars}`;
+    showModal();
+};
 
 const checkMatch = () => {
-    let { firstCard, secondCard } = gameState
+    let { firstCard, secondCard } = gameState;
     if (firstCard.dataset.name === secondCard.dataset.name) {
         // animate cards (match!)
-        firstCard.classList.add('bounce')
-        secondCard.classList.add('bounce')
-        firstCard.dataset.matched = true
-        secondCard.dataset.matched = true
-        gameState.setsMatched++
+        firstCard.classList.add('bounce');
+        secondCard.classList.add('bounce');
+        firstCard.dataset.matched = true;
+        secondCard.dataset.matched = true;
+        gameState.setsMatched++;
         if (gameState.setsMatched === 8) {
-            gameState.win = true
-            win()
+            gameState.win = true;
+            win();
         }
     } else {
         // animate cards (noMatch!)
-        firstCard.classList.add('wiggle')
-        secondCard.classList.add('wiggle')
+        firstCard.classList.add('wiggle');
+        secondCard.classList.add('wiggle');
         // requestAnimationFrame makes the css transition more consistent
         window.requestAnimationFrame(() => {
             setTimeout(() => {
-                firstCard.classList.remove('wiggle')
-                secondCard.classList.remove('wiggle')
-                firstCard.classList.toggle('flipped')
-                secondCard.classList.toggle('flipped')
-            }, 500)
-        })
+                firstCard.classList.remove('wiggle');
+                secondCard.classList.remove('wiggle');
+                firstCard.classList.toggle('flipped');
+                secondCard.classList.toggle('flipped');
+            }, 500);
+        });
     }
-    gameState.firstCard = null
-    gameState.secondCard = null
-    gameState.isChecking = false
-}
+    gameState.firstCard = null;
+    gameState.secondCard = null;
+    gameState.isChecking = false;
+};
 
 const flipBackCards = () => {
     for (let card of document.getElementsByClassName('card')) {
         if (card.dataset.matched) {
-            card.classList.toggle('flipped')
-            card.dataset.matched = false
+            card.classList.toggle('flipped');
+            card.dataset.matched = false;
         }
     }
     if (gameState.firstCard) {
-        gameState.firstCard.classList.toggle('flipped')
+        gameState.firstCard.classList.toggle('flipped');
     }
-}
+};
 
 const updateMovesAndStars = (moves) => {
     // This isn't ideal code, it's very verbose
-    moveCounter.textContent = moves === 1 ? `${moves} Move` : `${moves} Moves`
+    moveCounter.textContent = moves === 1 ? `${moves} Move` : `${moves} Moves`;
     if (moves < 17) {
-        gameState.stars = 3
+        gameState.stars = 3;
     } else if (moves < 22) {
-        gameState.stars = 2
+        gameState.stars = 2;
     } else {
-        gameState.stars = 1
+        gameState.stars = 1;
     }
     if (gameState.stars === 3) {
-        starOne.classList.add('filled-star')
-        starTwo.classList.add('filled-star')
-        starTwo.classList.remove('empty-star')
-        starThree.classList.add('filled-star')
-        starThree.classList.remove('empty-star')
+        starOne.classList.add('filled-star');
+        starTwo.classList.add('filled-star');
+        starTwo.classList.remove('empty-star');
+        starThree.classList.add('filled-star');
+        starThree.classList.remove('empty-star');
     }
     if (gameState.stars === 2) {
-        starThree.classList.remove('filled-star')
-        starThree.classList.add('empty-star')
+        starThree.classList.remove('filled-star');
+        starThree.classList.add('empty-star');
     }
     if (gameState.stars === 1) {
-        starTwo.classList.remove('filled-star')
-        starTwo.classList.add('empty-star')
+        starTwo.classList.remove('filled-star');
+        starTwo.classList.add('empty-star');
     }
-}
+};
 
 const updateTimer = time => {
-    let defaultText = time.getSeconds() + '<span class="time-unit">s</span>'
+    let defaultText = time.getSeconds() + '<span class="time-unit">s</span>';
     timer.innerHTML = time.getMinutes() ?
         `${time.getMinutes()}<span class="time-unit">m</span> ${defaultText}` :
-        defaultText
-}
+        defaultText;
+};
 
 const startTimer = () => {
     gameState.interval = setInterval(() => {
-        gameState.timer.setSeconds(gameState.timer.getSeconds() + 1)
-        updateTimer(gameState.timer)
-    }, 1000)
-}
+        gameState.timer.setSeconds(gameState.timer.getSeconds() + 1);
+        updateTimer(gameState.timer);
+    }, 1000);
+};
 
 const reset = () => {
-    flipBackCards()
-    if (!gameState.win) {
-        clearInterval(gameState.interval)
-    }
-    gameState = { ...initialGameState }
-    // date object is acting weird so I'm hard resetting timer
-    gameState.timer = new Date(0)
+    flipBackCards();
 
-    updateTimer(gameState.timer)
-    updateMovesAndStars(gameState.moves)
-    setTimeout(generateCards, 500)
-}
+    if (!gameState.win) {
+        clearInterval(gameState.interval);
+    }
+
+    gameState = { ...initialGameState };
+    // date object is acting weird so I'm hard resetting timer
+    gameState.timer = new Date(0);
+
+    updateTimer(gameState.timer);
+    updateMovesAndStars(gameState.moves);
+    setTimeout(generateCards, 500);
+};
 
 const beginGame = () => {
     // Create cards
-    generateCards()
+    generateCards();
 
     // Set initial values
-    updateMovesAndStars(gameState.moves)
-    updateTimer(gameState.timer)
+    updateMovesAndStars(gameState.moves);
+    updateTimer(gameState.timer);
 
     // Add Event Listeners
-    resetBtn.addEventListener('click', reset)
+    resetBtn.addEventListener('click', reset);
 
     playAgain.addEventListener('click', () => {
-        hideModal()
-        reset()
-    })
+        hideModal();
+        reset();
+    });;
 
     window.addEventListener('click', (e) => {
         if (e.target === modal) {
-            hideModal()
+            hideModal();
         }
-    })
-}
+    });
+};
 
 const showModal = () => {
-    modal.style.display = 'flex'
-    modal.classList.add('fade-in')
-    modal.classList.remove('hide')
-}
+    modal.style.display = 'flex';
+    modal.classList.add('fade-in');
+    modal.classList.remove('hide');
+};
 
 const hideModal = () => {
-    modal.classList.remove('fade-in')
-    modal.classList.add('hide')
+    modal.classList.remove('fade-in');
+    modal.classList.add('hide');
     // trigger display none slightly before hide animation ends
-    setTimeout(() => modal.style.display = 'none', 465)
-}
+    setTimeout(() => modal.style.display = 'none', 465);
+};
 
 // Take care of creating cards setting values and adding event listeners
-beginGame()
+beginGame();
 
 
